0c69c6e7ab5721634aa5682a7d80d3241307772c
Mason Hargrove <maseh87@gmail.com>
2015-06-09 06:15:31 -0700

    First commit with Greg

---
+++ server.js
@@ -0,0 +1,6 @@
+// GET - asking the server to give you some data
+// PUT - asking the server to update something
+// POST - sending the server some data to store
+// DELETE - asking the server to delete something
+// OPTIONS - made by the browser by default, verifies that the url is the right
+
