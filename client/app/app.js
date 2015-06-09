3d9ced823823f6a819eaced60fd331389211f309
Mason Hargrove <maseh87@gmail.com>
2015-06-09 07:42:02 -0700

    another commit

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 // Our main angular module
 var app = angular.module('app', []);
 
-app.controller('AppController', function($scope) {
+app.controller('AppController', function($scope, $http) {
   $scope.showHeader = false;
   
   // Storage for our todos! So our girlfriends dont yell at us
@@ -15,9 +15,15 @@ app.controller('AppController', function($scope) {
 
 
   $scope.deleteTodo = function(index) {
-    $scope.todos.splice(index, 1);
+    // $scope.todos.splice(index, 1);
   };
 
+  $scope.getData = function() {
+    $http.get('http://www.reddit.com/.json').then(function(result) {
+      console.log(result.data);
+      $scope.todos = result.data.data.children
+    });
+  }
 
 });
 
